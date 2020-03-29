67a8172f81f7ef12493767d50ffa096b6f631f68
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-03-29 12:44:36 +0200

    add hello endpoint

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname + '/public')));
+
 app.use((req, res, next ) => {
     res.show = (name) => {
         res.sendFile(path.join(__dirname + `/views/${name}`));
@@ -30,7 +32,11 @@ app.get('/history', (req, res) => {
     res.show('history.html');
 });
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.get('/hello/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+  });
+
+
 
 app.use((req, res) => {
     res.status(404).send('404 not found...')
