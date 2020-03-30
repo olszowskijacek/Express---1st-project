f438191ff4601a62cd68247857bf4f6134c4cd50
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-03-30 12:16:25 +0200

    add new endpoint & middleware

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -33,6 +34,10 @@ app.get('/history', (req, res) => {
 app.get('/hello/:name', (req, res) => {
     res.render('hello', { layout: false, name: req.params.name });
   });
+
+app.post('/contact/send-message', (req, res) => {
+    res.json(req.body);
+});
 
 app.use((req, res) => {
     res.status(404).send('404 not found...')
